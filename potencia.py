bcbab3a6b503b103e5c42eafc25f22e322308a1b
Daniel Vega <danielvegac2000@mail.com>
2020-02-27 18:37:43 -0500

    archivo inicial

---
+++ potencia.py
@@ -0,0 +1,12 @@
+import sys
+
+obtenerValores = sys.argv[1:]
+
+def obtenerLenguaje(lenguaje):
+    lenguaje = lenguaje[3:];
+    lenguaje.split(",");
+    lenguaje[-1] = str(lenguaje[-1]).replace("}","");
+    return lenguaje;
+
+print(obtenerLenguaje(str(obtenerValores[0])));
+
