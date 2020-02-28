c0cfa61f47991405b7b2b2e4975f36bc370e4ce5
andres <andrescasfe2001@gmail.com>
2020-02-27 19:25:51 -0500

    itent

--- potencia.py
+++ potencia.py
@@ -2,30 +2,44 @@ import sys
 
 obtenerValores = sys.argv[1:]
 
+
 def obtenerLenguaje(lenguaje):
     lenguaje = lenguaje[3:];
-    elementos=lenguaje.split(",");
-    elementos[-1] = elementos[-1].replace("}","")
+    elementos = lenguaje.split(",");
+    elementos[-1] = elementos[-1].replace("}", "")
     return elementos;
+
 
 def obtenerPotencia(potencia):
     potencia = potencia[2:]
     portencia = int(potencia);
     return potencia
 
+
 def obtenerVista(vista):
     vista = vista[0]
     return vista;
 
-print(obtenerLenguaje(obtenerValores[0]));
-print(obtenerPotencia(obtenerValores[1]))
-print(obtenerVista(obtenerValores[2]))
 
-for i in range(len(elementos())):
+elementos = obtenerLenguaje(obtenerValores[0]);
+potencia = obtenerPotencia(obtenerValores[1]);
+vista = obtenerVista(obtenerValores[2]);
 
-    if (elementos[i] == '#'):
-        
-        c +=c+1
-        print('#')
 
-        
+def productoCruz(elementos):
+    vectores = []
+    texto = "";
+    if (vista == "-"):
+        if (potencia == 0):
+            print("#");
+        else:
+            for i in range(potencia):
+
+                for q in i * 2:
+                    texto += elementos[i] + elementos[q]
+
+
+
+    else:
+        if (potencia == 0):
+            print("L1 = #");
